0834ece3a24afe7e36863abe15563b98ed9e76d2
sudipta das <dassuva02@gmail.com>
2019-06-16 22:32:16 +0600

    Intitial files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+function Dasboard_index() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
+}
+
+export default Dasboard_index;
---
+++ src/Custom_Code/Dashboard_index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Dashboard_index/>
+    </div>
+  );
+}
+
+export default App;
